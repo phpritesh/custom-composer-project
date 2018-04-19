5d491c42792d8ec72a69a6c45aa0599414696b41
ritesh <phpritesh@gmail.com>
2018-04-19 01:54:26 -0400

    initial commit.

---
+++ composr/src/Ritesh/Detact/Face.php
@@ -0,0 +1,12 @@
+<?php namespace Ritesh\Detact;
+/**
+ * Face Abstract Class
+ *
+ * @package    Face
+ * @author     Ridvan Baluyos <ridvan@baluyos.net>
+ * @link       https://github.com/ridvanbaluyos/face
+ * @license    MIT
+ */
+abstract class Face {
+
+}
